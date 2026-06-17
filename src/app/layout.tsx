ab1bffa2923e7eab8361135e27b9156d8fe64ad3
Ahmet CINAR <ahmetcinar1283@gmail.com>
2026-06-17 19:55:12 +0300

    hata düzeltildi layout yine

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
       suppressHydrationWarning
     >
     
-      <body>
+      <body suppressHydrationWarning>
         <ThemeProvider>
           {children}
           <CookieBanner />
